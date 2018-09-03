29844b5f75d2e25bf42a3597906247103d960abf
Unknown <rostyslav.doskochynskiy@gmail.com>
2018-09-03 11:36:58 +0300

    add signInForm

---
+++ src/components/routes/AdminPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AdminPage extends Component {
+    render() {
+        return (
+            <h1>Admin Page</h1>
+        );
+    }
+}
+
+export default AdminPage;
