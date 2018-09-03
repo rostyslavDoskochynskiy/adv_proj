ecba65f3bf83ad11648ca055d14bb8c5c1d9a9ed
Unknown <rostyslav.doskochynskiy@gmail.com>
2018-09-03 11:02:25 +0300

    initial configs

---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+    render() {
+        return (
+            <div>something root</div>
+        );
+    }
+}
+
+export default Root;
